6611d26225ac1fb8bdcd1f589dc085ffa37dc258
bwaklog <aditya.mh@outlook.com>
2024-01-06 17:05:31 +0530

    presenting yake(ಯಾಕೆ)

---
+++ README.md
@@ -0,0 +1,18 @@
+# yake lang
+[Brainfuck](https://en.wikipedia.org/wiki/Brainfuck) lang but shittier (ongoing proj.)
+
+```zsh
+$ ./yake "++<<"
+8
+```
+
+Operations
+
+Starting value : `0`
+
+|sym|opr description|
+|---|---|
+|<kbd>+</kbd>|Increment|
+|<kbd>-</kbd>|Decrement|
+|<kbd><</kbd>|Left Shift|
+|<kbd>><kbd>|Right Shift|
---
+++ yake.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int opr(char *v);
+void ex(int *p, char *syn);
+
+int main(int ac, char *av[]) {
+    int p = 0;
+    return (ac < 2) ? (printf("synerr\n"),1) : (ex(&p, av[1]), 0);
+}
+
+void ex(int *v, char *syn){
+    while ((int)*syn!='\0'){
+        switch ((int)*syn) {
+            case 43: ++*v; break;
+            case 45: --*v; break;
+            case 60: *v = *v<<1; break;
+            case 62: *v = *v>>1; break;
+            default: printf("synerr\n"); return;
+        }
+        syn++;
+    }
+    printf("%d\n", *v);
+    return;
+}
